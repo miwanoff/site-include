8085693bef61ad2cfe840b1e826773767562e34b
miwanoff <marina.iva.noffa@gmail.com>
2024-10-27 13:01:42 +0200

    Add math lib

---
+++ math.php
@@ -0,0 +1,3 @@
+<?php
+require_once "my_math.php";
+echo math_cube(235);
---
+++ my_math.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *Бібліотека функцій для математичних обчислень
+ *@author Автор
+ */
+/* Зведення у квадрат */
+function math_square($n)
+{
+    return $n * $n;
+}
+
+/* Зведення у куб */
+function math_cube($n)
+{
+    return $n * $n * $n;
+}
